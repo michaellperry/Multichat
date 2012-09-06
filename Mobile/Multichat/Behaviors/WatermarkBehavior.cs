5f62733067d56d82ae74e49b9a4268640e7df3e2
Michael L Perry <michael@qedcode.com>
2012-09-05 22:17:33 -0500

    Added watermark for message.

---
+++ Mobile/Multichat/Behaviors/WatermarkBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Multichat.Behaviors
+{
+    public class WatermarkBehavior : Behavior<TextBlock>
+    {
+        public static DependencyProperty WatermarkedTextBoxProperty =
+            DependencyProperty.Register(
+                "WatermarkedTextBox", 
+                typeof(TextBox), 
+                typeof(WatermarkBehavior), 
+                new PropertyMetadata(null));
+
+        public TextBox WatermarkedTextBox
+        {
+            get { return (TextBox)GetValue(WatermarkedTextBoxProperty); }
+            set { SetValue(WatermarkedTextBoxProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            TextBox watermarkedTextBox = WatermarkedTextBox;
+            if (watermarkedTextBox != null)
+            {
+                watermarkedTextBox.GotFocus += WatermarkedControl_GotFocus;
+                watermarkedTextBox.LostFocus += WatermarkedControl_LostFocus;
+                VisibleIfTextEmpty(watermarkedTextBox);
+            }
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            TextBox watermarkedTextBox = WatermarkedTextBox;
+            if (watermarkedTextBox != null)
+            {
+                watermarkedTextBox.GotFocus -= WatermarkedControl_GotFocus;
+                watermarkedTextBox.LostFocus -= WatermarkedControl_LostFocus;
+            }
+            base.OnDetaching();
+        }
+
+        void WatermarkedControl_GotFocus(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Visibility = Visibility.Collapsed;
+        }
+
+        void WatermarkedControl_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox watermarkedTextBox = WatermarkedTextBox;
+            if (watermarkedTextBox != null)
+                VisibleIfTextEmpty(watermarkedTextBox);
+        }
+
+        private void VisibleIfTextEmpty(TextBox hintedControl)
+        {
+            AssociatedObject.Visibility = String.IsNullOrWhiteSpace(hintedControl.Text)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+    }
+}
